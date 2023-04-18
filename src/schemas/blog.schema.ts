b9316f33ae58ec806b36ffeb57ecf7e07c3119d7
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-04-18 14:39:36 +0300

    Added blog schema

---
+++ src/schemas/blog.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type BlogDocument = HydratedDocument<Blog>;
+
+@Schema()
+export class Blog {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  websiteUrl: string;
+
+  @Prop({ required: true })
+  createdAt: Date;
+
+  @Prop({ required: true })
+  isMembership: boolean;
+}
+
+export const BlogSchema = SchemaFactory.createForClass(Blog);
